92f6c76e2c563265cb54b6ea11727f661905804a
mayank.tomar <mayank.tomar@u-next.com>
2023-02-11 18:13:39 +0530

    Deployment2

---
+++ api/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { routes } from './routes.js';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const databaseUrl = 'mongodb+srv://eduonix:Eduonix123@cluster0.ojryh6q.mongodb.net/customer';
+mongoose.connect(databaseUrl);
+const database = mongoose.connection;
+
+database.on('error',(error)=>{
+  console.log("Error while connecting to database");
+})
+
+database.once('connected',()=>{
+  console.log("Connected to database successfully");
+})
+
+app.use(routes)
+
+app.get('/healthcheck',(req,res)=>{
+  res.status(200).send("Server is up and running..!!");
+})
+
+app.listen(5000,()=>{
+  console.log("Server is running on port 5000");
+})
